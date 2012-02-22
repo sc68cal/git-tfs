38ac51af8acb5a9dc8964c3c07a857c59f78524a
Sean M. Collins <sean@coreitpro.com>
2012-02-22 10:53:57 -0500

    Unit test for RCheckin

---
+++ GitTfsTest/Commands/RCheckinTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
+using Sep.Git.Tfs.Commands;
+using Sep.Git.Tfs.Core;
+using Sep.Git.Tfs.Core.TfsInterop;
+using Sep.Git.Tfs.Test.TestHelpers;
+using StructureMap.AutoMocking;
+
+namespace Sep.Git.Tfs.Test.Commands
+{
+    [TestClass]
+    public class RCheckinTest
+    {
+        private RhinoAutoMocker<Rcheckin> mocks;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mocks = new RhinoAutoMocker<Rcheckin>();
+            mocks.Inject<TextWriter>(new StringWriter());
+            mocks.Get<Globals>().Repository = mocks.Get<IGitRepository>();
+        }
+
+        [TestMethod]
+        public void ShouldAssociateWithWorkItem()
+        {
+            mocks.Get<Globals>().Repository = mocks.Get<IGitRepository>();
+            var remote = mocks.Get<IGitTfsRemote>();
+            mocks.Get<IGitRepository>().Stub(x => x.GetLastParentTfsCommits(null)).IgnoreArguments()
+                .Return(new[] { new TfsChangesetInfo { Remote = remote } });
+            mocks.Get<IGitRepository>().Stub(x => x.GetLastParentTfsCommits("my-head")).Return(new TfsChangesetInfo[0]);
+            mocks.ClassUnderTest.Run();
+
+        }
+    }
+}
